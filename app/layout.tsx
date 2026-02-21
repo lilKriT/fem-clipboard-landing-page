4eb94b0226988235f94c917900d5294c486a01c9
lilKriT <lilKriTwebDev@gmail.com>
2026-02-21 20:33:27 +0100

    Added files, styles etc.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Bai_Jamjuree } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const baiJamjuree = Bai_Jamjuree({
+  variable: "--font-bai-jamjuree",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "600"],
 });
 
 export const metadata: Metadata = {
@@ -24,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${baiJamjuree.variable} antialiased`}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,78 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <p>A history of everything you copy</p>
+
+      <p>
+        Clipboard allows you to track and organize everything you copy.
+        Instantly access your clipboard on all your devices.
+      </p>
+
+      <p>Download for iOS Download for Mac</p>
+
+      <p>Keep track of your snippets</p>
+
+      <p>
+        Clipboard instantly stores any item you copy in the cloud, meaning you
+        can access your snippets immediately on all your devices. Our Mac and
+        iOS apps will help you organize everything.
+      </p>
+
+      <p>Quick Search</p>
+
+      <p>
+        Easily search your snippets by content, category, web address,
+        application, and more.
+      </p>
+
+      <p>iCloud Sync</p>
+
+      <p>Instantly saves and syncs snippets across all your devices.</p>
+
+      <p>Complete History</p>
+
+      <p>
+        Retrieve any snippets from the first moment you started using the app.
+      </p>
+
+      <p>Access Clipboard anywhere</p>
+
+      <p>
+        Whether you’re on the go, or at your computer, you can access all your
+        Clipboard snippets in a few simple clicks.
+      </p>
+
+      <p>Supercharge your workflow</p>
+
+      <p>We’ve got the tools to boost your productivity.</p>
+
+      <p>Create blacklists</p>
+
+      <p>
+        Ensure sensitive information never makes its way to your clipboard by
+        excluding certain sources.
+      </p>
+
+      <p>Plain text snippets</p>
+
+      <p>Remove unwanted formatting from copied text for a consistent look.</p>
+
+      <p>Sneak preview</p>
+
+      <p>Quick preview of all snippets on your Clipboard for easy access.</p>
+
+      <p>Clipboard for iOS and Mac OS</p>
+
+      <p>
+        Available for free on the App Store. Download for Mac or iOS, sync with
+        iCloud and you’re ready to start adding to your clipboard.
+      </p>
+
+      <p>Download for iOS Download for Mac</p>
+
+      <p>FAQs Contact Us Privacy Policy Press Kit Install Guide</p>
+    </div>
+  );
 }
